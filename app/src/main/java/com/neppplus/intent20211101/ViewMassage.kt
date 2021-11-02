aa91068ef738dd8a3cdc8286d90886283bfe083a
ujin-Bang <qkddnwls14@gmail.com>
2021-11-02 17:41:13 +0900

    넘어간 화면에서 -> 첨부한 String을 받아서 -> 텍스트뷰에 반영

--- app/src/main/java/com/neppplus/intent20211101/ViewMassage.kt
+++ app/src/main/java/com/neppplus/intent20211101/ViewMassage.kt
@@ -2,10 +2,15 @@ package com.neppplus.intent20211101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_massage.*
 
 class ViewMassage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_massage)
+
+        val recoiveMessage = intent.getStringExtra("message")
+
+        txtMessage.text = recoiveMessage
     }
 }
