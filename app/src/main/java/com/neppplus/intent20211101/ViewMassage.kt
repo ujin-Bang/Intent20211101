add4f2153ef4e635282e674208a73c41b4748e6e
ujin-Bang <qkddnwls14@gmail.com>
2021-11-02 17:17:26 +0900

    전달받을 내용

---
+++ app/src/main/java/com/neppplus/intent20211101/ViewMassage.kt
@@ -0,0 +1,11 @@
+package com.neppplus.intent20211101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMassage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_massage)
+    }
+}
