116754cde7a02bc4280def053878e9a366748768
ujin-Bang <qkddnwls14@gmail.com>
2021-11-02 16:54:17 +0900

    다른화면추가

---
+++ app/src/main/java/com/neppplus/intent20211101/OtherActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.intent20211101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+}
